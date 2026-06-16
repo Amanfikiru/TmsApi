164a035743198e5f37e854845eb4f2aa968a32f7
Amanfikiru <Amanuelfikiru49@gmail.com>
2026-06-16 18:00:28 +0300

    New Feature: working with api authorization

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "CustomPlaceholder";
+    options.DefaultChallengeScheme = "CustomPlaceholder";
+})
+.AddScheme<Microsoft.AspNetCore.Authentication.AuthenticationSchemeOptions, MinimalAuthHandler>("CustomPlaceholder", null);
+
 builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 app.UseRouting();
-
-app.UseAuthentication(); 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapGet("/api/assessments/results", () => Results.Ok(new
@@ -14,6 +20,20 @@ app.MapGet("/api/assessments/results", () => Results.Ok(new
     courseCode = "CS-101",
     studentId = "S-001",
     letterGrade = "A"
-}));
+}))
+.RequireAuthorization();
 
 app.Run();
+
+public class MinimalAuthHandler : Microsoft.AspNetCore.Authentication.AuthenticationHandler<Microsoft.AspNetCore.Authentication.AuthenticationSchemeOptions>
+{
+    public MinimalAuthHandler(
+        Microsoft.Extensions.Options.IOptionsMonitor<Microsoft.AspNetCore.Authentication.AuthenticationSchemeOptions> options,
+        ILoggerFactory logger,
+        System.Text.Encodings.Web.UrlEncoder encoder) : base(options, logger, encoder) { }
+
+    protected override Task<Microsoft.AspNetCore.Authentication.AuthenticateResult> HandleAuthenticateAsync()
+    {
+        return Task.FromResult(Microsoft.AspNetCore.Authentication.AuthenticateResult.NoResult());
+    }
+}
